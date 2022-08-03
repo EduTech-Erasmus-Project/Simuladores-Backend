d1ab948405a6b9b69476f62669a2336e04c4f1c4
Claudio Mldo <claum0395@gmail.com>
2022-08-03 12:09:17 -0500

    refactor modules

--- adminApi/serializers.py
+++ adminApi/serializers.py
@@ -1,14 +1,6 @@
-from dataclasses import field
-from msilib.schema import Class
-from django.contrib.auth.models import update_last_login
 from rest_framework import serializers
 from SimuladoresLaboralesApi.serializers import UsuarioSerializer
-from SimuladoresLaboralesApi.serializers import CompetenciaTotal
-from SimuladoresLaboralesApi.serializers import Ejercitario
-from SimuladoresLaboralesApi.serializers import CompetenciaTotal
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.settings import api_settings
-from rest_framework_simplejwt.tokens import RefreshToken
+
 
 from usuario.models import Usuario, Participante, Evaluador
 
